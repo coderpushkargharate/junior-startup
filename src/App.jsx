434147da8d0a148977c7ef978e7c2a169e60d515
coderpushkargharate <pushkargharate7@gmail.com>
2024-11-18 15:48:58 +0530

    canges

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Bootstrap JavaScript (
 import './App.css'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    hello pushkar divya
+     <BrowserRouter>
+       <Header/>
+       <Routes>
+     
+
+       </Routes>
+       <Footer/>
+       </BrowserRouter>
     </>
   )
 }
