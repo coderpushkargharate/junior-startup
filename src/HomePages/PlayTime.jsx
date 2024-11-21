7fe453772b61952132f3f7c8b69b2e11eb5c4d39
Divya Gate <147638697+divyagat@users.noreply.github.com>
2024-11-21 12:26:49 +0530

    changes

---
+++ src/HomePages/PlayTime.jsx
@@ -0,0 +1,49 @@
+import "../HomePages/playtime.css"
+function PlayTime() {
+    return (
+        <>
+            <div className="container-fluid mt-5" >
+                <div className="container" id="Playtime">
+                    <div className="row">
+                        <div className="col-4 ">
+                            <div className="row">
+                                <div className="col-6 playimg">
+                                <img src="public/homeimg/h1-icon-2.png" alt="" />
+
+                                </div>
+                                <div className="col-6" >
+                                    <h3>playtime</h3>
+                                </div>
+                            </div>
+                            {/* <div className="d-flex ">
+                                <div className="me-5">
+                                    <img src="public/homeimg/h1-icon-2.png" alt="" />
+                                </div>
+                                <div className="ms-5">
+                                    <h3>play time</h3>
+                                </div>
+                            </div> */}
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati suscipit quis atque reiciendis vero, amet minima, molestiae totam doloribus itaque placeat officia praesentium temporibus aliquam cumque adipisci est ullam sunt.</p>
+                        </div>
+                        <div className="col-4">
+
+                        </div>
+                        <div className="col-4">
+
+                        </div>
+                        <div className="col-4">
+
+                        </div>
+                        <div className="col-4">
+
+                        </div>
+                        <div className="col-4">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default PlayTime
