bed7d6189509bf2e8ee0d825b71694b08feebcc1
Divya Gate <147638697+divyagat@users.noreply.github.com>
2024-11-21 22:43:35 +0530

    Changes

--- src/HomePages/PlayTime.jsx
+++ src/HomePages/PlayTime.jsx
@@ -9,11 +9,11 @@ function PlayTime() {
                         <div className="col-4 ">
                             <div className="row">
                                 <div className="col-6 playimg pb-3">
-                                <img src="public/homeimg/h1-icon-2.png" alt="" />
+                                    <img src="public/homeimg/h1-icon-2.png" alt="" />
 
                                 </div>
-                                <div className="col-6" >
-                                    <h3>playtime</h3>
+                                <div className="col-6 pt-4 pe-5 "  >
+                                    <h3>Play Time</h3>
                                 </div>
                             </div>
                             {/* <div className="d-flex ">
@@ -27,68 +27,68 @@ function PlayTime() {
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati suscipit quis atque reiciendis vero, amet minima, molestiae totam doloribus itaque placeat officia praesentium temporibus aliquam cumque adipisci est ullam sunt.</p>
                         </div>
                         <div className="col-4">
-                        <div className="row">
+                            <div className="row">
                                 <div className="col-6 playimg pb-3">
-                                <img src="public/homeimg/h1-icon-3.png" alt="" />
+                                    <img src="public/homeimg/h1-icon-3.png" alt="" />
 
                                 </div>
-                                <div className="col-6" >
+                                <div className="col-6  pt-4 pe-5" >
                                     <h3>Daycare</h3>
                                 </div>
                             </div>
-                    
+
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati suscipit quis atque reiciendis vero, amet minima, molestiae totam doloribus itaque placeat officia praesentium temporibus aliquam cumque adipisci est ullam sunt.</p>
                         </div>
                         <div className="col-4">
-                        <div className="row">
+                            <div className="row">
                                 <div className="col-6 playimg pb-3">
-                                <img src="public/homeimg/h1-icon-1.png" alt="" />
+                                    <img src="public/homeimg/h1-icon-1.png" alt="" />
 
                                 </div>
-                                <div className="col-6" >
+                                <div className="col-6 pt-4 pe-5" >
                                     <h3>Learning</h3>
                                 </div>
                             </div>
-                    
+
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati suscipit quis atque reiciendis vero, amet minima, molestiae totam doloribus itaque placeat officia praesentium temporibus aliquam cumque adipisci est ullam sunt.</p>
                         </div>
                         <div className="col-4">
-                        <div className="row">
+                            <div className="row">
                                 <div className="col-6 playimg pb-3">
-                                <img src="public/homeimg/h1-icon-4.png" alt="" />
+                                    <img src="public/homeimg/h1-icon-4.png" alt="" />
 
                                 </div>
-                                <div className="col-6" >
+                                <div className="col-6  pt-4 pe-5" >
                                     <h3>Outdoors</h3>
                                 </div>
                             </div>
-                    
+
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati suscipit quis atque reiciendis vero, amet minima, molestiae totam doloribus itaque placeat officia praesentium temporibus aliquam cumque adipisci est ullam sunt.</p>
                         </div>
                         <div className="col-4">
-                        <div className="row">
+                            <div className="row">
                                 <div className="col-6 playimg pb-3">
-                                <img src="public/homeimg/h1-icon-5.png" alt="" />
+                                    <img src="public/homeimg/h1-icon-5.png" alt="" />
 
                                 </div>
-                                <div className="col-6" >
+                                <div className="col-6 pt-4 pe-5" >
                                     <h3>Healthy Meals</h3>
                                 </div>
                             </div>
-                    
+
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati suscipit quis atque reiciendis vero, amet minima, molestiae totam doloribus itaque placeat officia praesentium temporibus aliquam cumque adipisci est ullam sunt.</p>
                         </div>
                         <div className="col-4">
-                        <div className="row">
+                            <div className="row">
                                 <div className="col-6 playimg pb-3">
-                                <img src="public/homeimg/h1-icon-6.png" alt="" />
+                                    <img src="public/homeimg/h1-icon-6.png" alt="" />
 
                                 </div>
-                                <div className="col-6" >
+                                <div className="col-6 pt-4 pe-5" >
                                     <h3>Events</h3>
                                 </div>
                             </div>
-                    
+
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati suscipit quis atque reiciendis vero, amet minima, molestiae totam doloribus itaque placeat officia praesentium temporibus aliquam cumque adipisci est ullam sunt.</p>
                         </div>
                     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import { FaBaseballBall } from "react-icons/fa";
 import Shop from "./Shop";
 import PlayTime from "../HomePages/PlayTime";
+import Learningpage from "../HomePages/Learningpage";
+import LatestEvents from "../HomePages/LatestEvents";
 
 
 function Home() {
   return (
     <>
         <PlayTime/>
+        <Learningpage/>
+        <LatestEvents/>
     </>
   )
 }
