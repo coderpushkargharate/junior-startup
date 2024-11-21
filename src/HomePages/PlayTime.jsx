b0c33033b1440da69b5a9e26bdb7e1ab9871aac9
Divya Gate <147638697+divyagat@users.noreply.github.com>
2024-11-21 12:41:43 +0530

    changes

--- src/HomePages/PlayTime.jsx
+++ src/HomePages/PlayTime.jsx
@@ -26,19 +26,69 @@ function PlayTime() {
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati suscipit quis atque reiciendis vero, amet minima, molestiae totam doloribus itaque placeat officia praesentium temporibus aliquam cumque adipisci est ullam sunt.</p>
                         </div>
                         <div className="col-4">
+                        <div className="row">
+                                <div className="col-6 playimg">
+                                <img src="public/homeimg/h1-icon-3.png" alt="" />
 
+                                </div>
+                                <div className="col-6" >
+                                    <h3>Daycare</h3>
+                                </div>
+                            </div>
+                    
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati suscipit quis atque reiciendis vero, amet minima, molestiae totam doloribus itaque placeat officia praesentium temporibus aliquam cumque adipisci est ullam sunt.</p>
                         </div>
                         <div className="col-4">
+                        <div className="row">
+                                <div className="col-6 playimg">
+                                <img src="public/homeimg/h1-icon-1.png" alt="" />
 
+                                </div>
+                                <div className="col-6" >
+                                    <h3>Learning</h3>
+                                </div>
+                            </div>
+                    
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati suscipit quis atque reiciendis vero, amet minima, molestiae totam doloribus itaque placeat officia praesentium temporibus aliquam cumque adipisci est ullam sunt.</p>
                         </div>
                         <div className="col-4">
+                        <div className="row">
+                                <div className="col-6 playimg">
+                                <img src="public/homeimg/h1-icon-4.png" alt="" />
 
+                                </div>
+                                <div className="col-6" >
+                                    <h3>Outdoors</h3>
+                                </div>
+                            </div>
+                    
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati suscipit quis atque reiciendis vero, amet minima, molestiae totam doloribus itaque placeat officia praesentium temporibus aliquam cumque adipisci est ullam sunt.</p>
                         </div>
                         <div className="col-4">
+                        <div className="row">
+                                <div className="col-6 playimg">
+                                <img src="public/homeimg/h1-icon-5.png" alt="" />
 
+                                </div>
+                                <div className="col-6" >
+                                    <h3>Healthy Meals</h3>
+                                </div>
+                            </div>
+                    
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati suscipit quis atque reiciendis vero, amet minima, molestiae totam doloribus itaque placeat officia praesentium temporibus aliquam cumque adipisci est ullam sunt.</p>
                         </div>
                         <div className="col-4">
+                        <div className="row">
+                                <div className="col-6 playimg">
+                                <img src="public/homeimg/h1-icon-6.png" alt="" />
 
+                                </div>
+                                <div className="col-6" >
+                                    <h3>Events</h3>
+                                </div>
+                            </div>
+                    
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati suscipit quis atque reiciendis vero, amet minima, molestiae totam doloribus itaque placeat officia praesentium temporibus aliquam cumque adipisci est ullam sunt.</p>
                         </div>
                     </div>
                 </div>
