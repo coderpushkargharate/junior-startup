a0b5e48632aff6a21a00d96c75dbd183bc74d452
coderpushkargharate <pushkargharate7@gmail.com>
2024-11-22 16:59:32 +0530

    dftg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { FaSearch } from "react-icons/fa";
+import "../components/header.css"
 const Header = () => (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container">
-            <Link className="navbar-brand" to="/">Navbar</Link>
+    <nav className="navbar navbar-expand-lg  header " >
+        <div className="container-fluid px-5 py-2 header">
+            <Link className="navbar-brand" to="/"><img src="public/homeimg/width_200.png" alt="" /></Link>
             <button
                 className="navbar-toggler"
                 type="button"
@@ -17,44 +18,32 @@ const Header = () => (
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <ul className="navbar-nav ms-auto  mb-lg-0 ">
                     <li className="nav-item">
-                        <Link className="nav-link active" to="/home">Home</Link>
+                        <Link className="nav-link active me-4 navc text-white" to="/home">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/courses">Courses</Link>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a
-                            className="nav-link dropdown-toggle"
-                            href="#"
-                            role="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                        >
-                            Dropdown
-                        </a>
-                        <ul className="dropdown-menu">
-                            <li><Link className="dropdown-item" to="/action">Action</Link></li>
-                            <li><Link className="dropdown-item" to="/another-action">Another action</Link></li>
-                            <li><hr className="dropdown-divider" /></li>
-                            <li><Link className="dropdown-item" to="/something-else">Something else here</Link></li>
-                        </ul>
+                        <Link className="nav-link active me-4 navc text-white" to="/courses">Courses</Link>
                     </li>
                     <li className="nav-item">
-                        <span className="nav-link disabled" aria-disabled="true">Disabled</span>
+                        <Link className="nav-link active me-4 navc text-white" to="/courses">Events</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link active me-4 navc text-white" to="/courses">Pages</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link active me-4 navc text-white" to="/courses">Shop</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link active me-4 navc text-white" to="/courses">Blog</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link active me-4 navc text-white" to="/courses">Elements</Link>
                     </li>
                 </ul>
-                <form className="d-flex" role="search">
-                    <input
-                        className="form-control me-2"
-                        type="search"
-                        placeholder="Search"
-                        aria-label="Search"
-                    />
-                    <button className="btn btn-outline-success" type="submit">
-                        Search
-                    </button>
+                <form className="d-flex text-white" role="search">
+                    <FaSearch className='text-white' />
+
                 </form>
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
   return (
     <>
 
-      <div className="container-fluid ">
+      <div className="container-fluid pt-5">
 
         <div className="row">
           <div className="col-12">
